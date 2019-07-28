1187d963d55bb39fb182d4c80b82d1fb14c18cd2
Cass <m@cass.si>
2019-07-28 22:00:05 +0100

    hardware code done

--- hardware/main.py
+++ hardware/main.py
@@ -6,7 +6,7 @@ from board import SCL, SDA
 from adafruit_seesaw.seesaw import Seesaw
 
 PLANT_UUID = ""
-URL = "http://something.com/"+PLANT_UUID
+URL = "https://moisture-track.herokuapp.com/plants/"+PLANT_UUID
 
 i2c_bus = busio.I2C(SCL, SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
@@ -14,9 +14,14 @@ ss = Seesaw(i2c_bus, addr=0x36)
 while True:
   # read moisture level through capacitive touch pad
   touch = ss.moisture_read()
-  # read temperature from the temperature sensor
-  temp = ss.get_temp()
-  print("temp: " + str(temp) + "  moisture: " + str(touch))
+
+  # send moisture update to the api
+  payload = {
+  	"moisture_level": str(touch)
+  }
+	requests.put(URL, data=payload)
 
 	#repeat once every 30 minutes
 	timer.sleep(1800)
+
+#https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/python-circuitpython-test
