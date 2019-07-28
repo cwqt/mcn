35910f71a3b6b5009e5457899b702f061b90101f
Cass <m@cass.si>
2019-07-28 21:10:52 +0100

    preview

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,7 @@ const store = createStore(
 	compose(
 		applyMiddleware(...middleware),
 		//allow redux chrome extension to work
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
