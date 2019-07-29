3a72ffa41279e9875455e14a6b5606a333613210
Cass <m@cass.si>
2019-07-29 11:45:52 +0100

    calling wrong README.md

--- api/app.py
+++ api/app.py
@@ -19,7 +19,7 @@ api = Api(app)
 
 @app.route('/')
 def get_docs():
-  with open(os.path.dirname(app.root_path) + "/README.md", "r") as markdown_file:
+  with open("README.md", "r") as markdown_file:
     content = markdown_file.read()
     return markdown.markdown(content)
 
