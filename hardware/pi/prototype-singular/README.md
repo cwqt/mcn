95715f7249333b5f356a694057c81ab678d161cc
Cass <m@cass.si>
2020-01-27 14:21:27 +0000

    add pi py file

--- hardware/pi/prototype-singular/README.md
+++ hardware/pi/prototype-singular/README.md
@@ -1,10 +1,10 @@
 # Installation
 
 ```
-sudo apt-get install python3-pip
-pipenv --python 3.7
+sudo apt-get install python3-pip ttf-dejavu python3-pil
 
-sudo apt-get install ttf-dejavu
-sudo apt-get install python3-pil
+git clone https://gitlab.com/cxss/moisture.track.git
 pipenv install
 ```
+
+Uses an arduino nano as and i2c adc slave because I didn't have any mcp3008's lying about.
--- hardware/pi/prototype-singular/main.py
+++ hardware/pi/prototype-singular/main.py
@@ -1,2 +1,58 @@
 from time import sleep
 
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+disp = ili9341.ILI9341(spi, rotation=90, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width   # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width   # we swap height/width to rotate it to landscape!
+    height = disp.height
+image = Image.new('RGB', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image)
+
+image = Image.open("blinka.jpg")
+
+# Scale the image to the smaller screen dimension
+image_ratio = image.width / image.height
+screen_ratio = width / height
+if screen_ratio < image_ratio:
+    scaled_width = image.width * height // image.height
+    scaled_height = height
+else:
+    scaled_width = width
+    scaled_height = image.height * width // image.width
+image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+x = scaled_width // 2 - width // 2
+y = scaled_height // 2 - height // 2
+image = image.crop((x, y, x + width, y + height))
+
+# Display image.
+disp.image(image)
