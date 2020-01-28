e4eee3a3da5d7e3dc765f28804407fc48eb32621
Cass <m@cass.si>
2020-01-28 15:59:57 +0000

    spaces

--- hardware/pi/prototype-singular/main.py
+++ hardware/pi/prototype-singular/main.py
@@ -14,7 +14,8 @@ PLANT_UUID = os.getenv("PLANT_UUID")
 
 UPDATE_PERIOD = 3600
 STEMMA_ADDRESS = 0x36
-LDR_ADDRESS = 0x06
+LDR_ADDRESS = 0x00
+BUTTON_ADDRESS = board.D13
 
 HEADERS = {
   "x-api-key": API_KEY,
@@ -25,35 +26,38 @@ URL = API_URL+"/plants/"+PLANT_UUID
 i2c_bus = busio.I2C(SCL, SDA)
 stemma = Seesaw(i2c_bus, addr=STEMMA_ADDRESS)
 
-pixels = neopixel.NeoPixel(board.D17, 1)
+pixels = neopixel.NeoPixel(board.D12, 1)
 pixels[0] = (255, 0, 0)
 
 def get_light():
-	return 1
+  return 1
 
 def post():
-	moisture 		= stemma.moisture_read()
-	temperature = stemma.get_temp()
-	light 			= get_light() 
+  moisture    = stemma.moisture_read()
+  temperature = stemma.get_temp()
+  light       = get_light() 
 
-	payload = {
-		"moisture": 	 moisture,
-		"temperature": temperature,
-		"light": 			 light
-	}
+  payload = {
+    "moisture":    moisture,
+    "temperature": temperature,
+    "light":       light
+  }
 
-	print(json.dumps(payload))
+  print(json.dumps(payload))
   r = requests.put(URL, data=json.dumps(payload), headers=HEADERS)
   return r.status_code
 
-while True:
-	pixels[0] = (0, 0, 255)
-	status = post()
+def post_w_led():
+  pixels[0] = (0, 0, 255)
+  status = post()
 
   if not status = requests.codes.ok:
-	  pixels[0] = (255,0,0)  	
+    pixels[0] = (255,0,0)   
   pixels[0] = (0,255,0)
 
   time.sleep(15)
   pixels[0] = (0,0,0)
-	time.sleep(UPDATE_PERIOD)
+
+while True:
+  post_w_led()
+  time.sleep(UPDATE_PERIOD)
