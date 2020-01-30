dd5248a776ce954c64c00639b73403eeea68a8ed
Cass <m@cass.si>
2020-01-30 16:23:58 +0000

    add letsencrypt route

--- api/__init__.py
+++ api/__init__.py
@@ -15,6 +15,7 @@ from resources.plant_list   import Plants
 from resources.plant        import Plant
 from resources.measurements import Measurements
 from resources.events       import Events
+from resources.letsencrypt  import LetsEncrypt
 
 def create_app(config_name):
   app = Flask(__name__)
@@ -35,5 +36,7 @@ def create_app(config_name):
 
   api.add_resource(Measurements, "/<path:obj_type>/<string:uuid>/measurements")
   api.add_resource(Events, "/<path:obj_type>/<string:uuid>/events")
+
+  api.add_resource(LetsEncrypt, "/.well-known/acme-challenge/Oa-mvfItbs3V9fXp9fXTyBsj5hGgqoB_05OzmZi215o")
 
   return app
